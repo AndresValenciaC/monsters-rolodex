9f24fe03c77e8b22d17bffd2fcbc49f334767430
Andres_Valencia <andres228cv@gmail.com>
2020-09-08 09:13:44 -0500

    Updates

---
+++ precache-manifest.339dd1cc795a6e827d1fdafceb72d383.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df0ba0077839e4e26062f714d9ece05",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "38c9ef42acc2881f1b78",
+    "url": "/monsters-rolodex/static/css/main.ef01dd64.chunk.css"
+  },
+  {
+    "revision": "bc4f0aec8753b31eee9d",
+    "url": "/monsters-rolodex/static/js/2.e601ea17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.e601ea17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38c9ef42acc2881f1b78",
+    "url": "/monsters-rolodex/static/js/main.27152530.chunk.js"
+  },
+  {
+    "revision": "99aeed3ea4362ed85205",
+    "url": "/monsters-rolodex/static/js/runtime-main.dcaacdb6.js"
+  }
+]);
